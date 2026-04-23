ef2aebd5412ac490bcceb3bbdba0a165b2484177
deeep8250 <deepmondal825005@gmail.com>
2026-04-23 20:45:04 +0530

    added auth services

---
+++ internal/auth/repository/Auth_repository.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/threadpulse/internal/models"
+)
+
+type AuthRepo struct {
+	DB *sqlx.DB
+}
+
+func NewAuthRepo(db *sqlx.DB) *AuthRepo {
+	return &AuthRepo{
+		DB: db,
+	}
+
+}
+
+func (r *AuthRepo) RegisterNewUserRepo(userRegister *models.User) error {
+
+	query := `insert into users (name,email,hashed_pass) values($1,$2,$3)`
+	_, err := r.DB.Exec(query, userRegister.Name, userRegister.Email, userRegister.HashPassword)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (r *AuthRepo) VerifyByEmail(email string) (*models.User, error) {
+	var user models.User
+	query := `select * from users where email=$1`
+	err := r.DB.Get(&user, query, email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, err
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
---
+++ internal/auth/services/Auth_service.go
@@ -0,0 +1,61 @@
+package service
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/threadpulse/internal/auth/repository"
+	"github.com/threadpulse/internal/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService struct {
+	AuthRepo *repository.AuthRepo
+}
+
+func NewAuthService(repo *repository.AuthRepo) *AuthService {
+	return &AuthService{
+		AuthRepo: repo,
+	}
+}
+
+func (s *AuthService) Register(registerInput models.Register) error {
+
+	existUser, err := s.AuthRepo.VerifyByEmail(registerInput.Email)
+	if existUser != nil {
+		return errors.New("user already exists")
+	} else if err != sql.ErrNoRows {
+		return err
+	}
+
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(registerInput.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	User := &models.User{
+		Name:         registerInput.Name,
+		Email:        registerInput.Email,
+		HashPassword: string(hashedPass),
+	}
+
+	err = s.AuthRepo.RegisterNewUserRepo(User)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *AuthService) Login(user models.Login) error {
+	ReturnedUser, err := s.AuthRepo.VerifyByEmail(user.Email)
+	if err != nil {
+		return err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(ReturnedUser.HashPassword), []byte(user.Password))
+	if err != nil {
+		return errors.New("invalid user")
+	}
+	return nil
+
+}
