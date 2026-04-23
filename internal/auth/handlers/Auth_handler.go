10c4ad3a20123de2d45d33314a1e850f410a53f7
deeep8250 <deepmondal825005@gmail.com>
2026-04-23 20:44:40 +0530

    added auth handlers

---
+++ internal/auth/handlers/Auth_handler.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	service "github.com/threadpulse/internal/auth/services"
+	"github.com/threadpulse/internal/models"
+)
+
+type AuthService struct {
+	services *service.AuthService
+}
+
+func NewAuthService(serv *service.AuthService) *AuthService {
+	return &AuthService{services: serv}
+}
+
+func (h *AuthService) RegisterHandler(c *gin.Context) {
+	var UserRegisterInput models.Register
+	err := c.ShouldBind(&UserRegisterInput)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = h.services.Register(UserRegisterInput)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"response": "user created",
+	})
+}
+func (h *AuthService) Login(c *gin.Context) {
+
+	var UserLoginInput models.Login
+
+	err := c.ShouldBindJSON(&UserLoginInput)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = h.services.Login(UserLoginInput)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "login-successfull"})
+
+}
