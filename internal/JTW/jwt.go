dbd0ad3f6d2804aa9cfdcdff50e96f2d0620307f
deeep8250 <deepmondal825005@gmail.com>
2026-04-24 07:00:20 +0530

    added middleware and jwt

---
+++ internal/JTW/jwt.go
@@ -0,0 +1,23 @@
+package jtw
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func JWTinit(userID int) (string, error) {
+
+	claims := jwt.MapClaims{
+		"userID": userID,
+		"exp_at": time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
---
+++ internal/middleware/middleware.go
@@ -0,0 +1,61 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+)
+
+func Miiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		HeaderValue := c.GetHeader("Authorization")
+		if HeaderValue == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "token is empty",
+			})
+			c.Abort()
+			return
+		}
+
+		parts := strings.Split(HeaderValue, " ")
+
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid token format",
+			})
+			c.Abort()
+			return
+		}
+
+		tokenstring := parts[0]
+
+		token, err := jwt.Parse(tokenstring, func(t *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "invlaid signature",
+			})
+			c.Abort()
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid claims in token",
+			})
+			c.Abort()
+			return
+		}
+
+		userID := int(claims["userID"].(float64))
+		c.Set("userID", userID)
+		c.Next()
+
+	}
+}
