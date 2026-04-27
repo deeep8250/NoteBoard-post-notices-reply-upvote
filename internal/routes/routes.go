3a630d8799f867880d9ebcee6a727dcbd25dba2e
deeep8250 <deepmondal825005@gmail.com>
2026-04-27 20:04:12 +0530

    added routes public/private for threads

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,14 +1,12 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	handler "github.com/threadpulse/internal/auth/handlers"
 	"github.com/threadpulse/internal/middleware"
 )
 
-func Routes(r *gin.Engine, auth *handler.AuthHandler) {
+func Routes(r *gin.Engine, auth *handler.AuthHandler, ThreadHandler *handler.ThreadHandler) {
 
 	authHandler := r.Group("/auth")
 	{
@@ -16,12 +14,17 @@ func Routes(r *gin.Engine, auth *handler.AuthHandler) {
 		authHandler.POST("/login", auth.Login)
 	}
 
-	Protected := r.Group("/protected")
-	Protected.Use(middleware.Miiddleware())
-	Protected.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "middleware and jwt worked",
-		})
+	Protected := r.Group("/private", middleware.Miiddleware())
+	{
+		Protected.POST("/thread", ThreadHandler.CreateThreadHandler)
+		Protected.PATCH("/thread/:id", ThreadHandler.UpdateThreadHandler)
+		Protected.DELETE("/thread/:id", ThreadHandler.DeleteThreadHandler)
 
-	})
+	}
+	Public := r.Group("/public")
+	{
+		Public.GET("/threads", ThreadHandler.GetAllThreadHandler)
+		Public.GET("/thread/:id", ThreadHandler.GetThreadByIdHandler)
+	}
+
 }
