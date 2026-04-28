db8511ece179825adc80a45095a4db014219e997
deeep8250 <deepmondal825005@gmail.com>
2026-04-28 19:12:55 +0530

    added handler layer for replies

--- internal/auth/handlers/Replies_handler.go
+++ internal/auth/handlers/Replies_handler.go
@@ -1,1 +1,186 @@
 package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	service "github.com/threadpulse/internal/auth/services"
+	"github.com/threadpulse/models"
+)
+
+type RepliesHandler struct {
+	service *service.RepliesService
+}
+
+func NewRepliesHandler(serv *service.RepliesService) *RepliesHandler {
+	return &RepliesHandler{
+		service: serv,
+	}
+}
+
+func (h *RepliesHandler) CreateRepliesHandler(c *gin.Context) {
+
+	postID := c.Param("id")
+	postIDint, err := strconv.Atoi(postID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid url parameter",
+		})
+		return
+	}
+
+	var reply models.Replies
+	err = c.ShouldBindJSON(&reply)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	userID, ok := c.Get("userID")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "unauthorized user",
+		})
+		return
+	}
+
+	reply.PostID = postIDint
+	reply.UserID = userID.(int)
+
+	err = h.service.CreateRepliesService(reply)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status": "reply added to post's comment section",
+	})
+
+}
+
+func (h *RepliesHandler) GetAllRepliesHandler(c *gin.Context) {
+
+	PostId := c.Param("id")
+	PostIdInt, err := strconv.Atoi(PostId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid url parameter",
+		})
+		return
+	}
+
+	limit := c.DefaultQuery("limit", "5")
+
+	limitInt, err := strconv.Atoi(limit)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid query parameter",
+		})
+		return
+	}
+	page := c.DefaultQuery("page", "1")
+	pageInt, err := strconv.Atoi(page)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid query parameter",
+		})
+		return
+	}
+
+	replies, count, err := h.service.GetAllRepliessService(PostIdInt, limitInt, pageInt)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"postID":  PostId,
+		"replies": replies,
+		"count":   count,
+	})
+
+}
+
+func (h *RepliesHandler) UpdateRepliesHandler(c *gin.Context) {
+	postID := c.Param("id")
+	PostIdInt, err := strconv.Atoi(postID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid url parameter",
+		})
+		return
+	}
+
+	userID, ok := c.Get("userID")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "unauthorized user",
+		})
+		return
+	}
+
+	var replyUpdated models.Replies
+	err = c.ShouldBindJSON(&replyUpdated)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	replyUpdated.PostID = PostIdInt
+	replyUpdated.UserID = userID.(int)
+
+	err = h.service.UpdateReplyService(replyUpdated)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status": "reply updated",
+	})
+
+}
+
+func (h *RepliesHandler) DeleteReplyHandler(c *gin.Context) {
+	replyID := c.Param("id")
+	replyIdInt, err := strconv.Atoi(replyID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid url parameter",
+		})
+		return
+	}
+
+	userID, ok := c.Get("userID")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "unauthorized user",
+		})
+		return
+	}
+
+	err = h.service.DeleteReplyService(replyIdInt, userID.(int))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "deleted the reply",
+	})
+
+}
